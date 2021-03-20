dc75c2730f2fa0e72276f7a840304c112875b3bb
Marcela Lyzandra <malymendes3010@gmail.com>
2021-03-19 23:40:29 -0300

    definicao de rotas, criacao de views e controlador

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EventoController;
+use App\Models\Evento;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home'); //ok
+
+Route::resource('eventos', EventoController::class);
